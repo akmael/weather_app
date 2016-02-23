49e0b2636c7049895f405be0ac11871786c5411d
AndraIshmael <ishmaelak@gmail.com>
2016-02-22 23:32:09 -0500

    90% done

--- public/js/app.js
+++ public/js/app.js
@@ -1,17 +1,18 @@
 var weatherData = {};
 $(document).ready(function($){
+
 $('#click').on('click', getWeather);//controls button
 var baseUrl = 'https://api.forecast.io/forecast/';
-console.log(geoplugin_countryName());
 function buildUrl(lat, lon){
       return baseUrl + apiKey+'/'+lat+','+lon;
 
     }
 
 
+
 //grabbing weatherData
 function getWeather(){
-
+     $('#click').fadeOut();//Make button go byebye
    var options = {
      url: buildUrl(geoplugin_latitude(),geoplugin_longitude()),
      dataType: 'jsonp',
@@ -27,12 +28,50 @@ function getWeather(){
 function errorHandler(err){
     console.log(err);
 }
+function whatYouDo(icon){
+     var compare = icon;
+
+     console.log(compare);
+     if( compare === 'clear-day'){
+
+          return "Go on a picnic or someting!";
+     }
+     else if(compare === 'clear-night'){
+
+           return "Stare at the night sky mabey find meaning in your pathetic life.";
+
+     }
+     else if(compare === 'cloudy'){
+           return 'Im drinking, good luck with your life.';
+     }
+     else if(compare === 'fog'){
+            return 'Stay inside, there might be some silent hill type mess outside.';
+     }
+     else if(compare === "partly-cloudy-night"){
+             return 'It s a wonderfull night for a moondance.... NOT!';
+     }
+     else if (compare === 'partly-cloudy-day'){
+              return 'Get your camera out its a good day to shoot for NatGeo!';
+     }
+     else if(compare === 'rain'){
+              return 'Netflix and chill, Hulu and commitent, or Youtube and saddness';
+     }
+     else if(compare === 'sleet'){
+              return '....F@&# That.'
+     }
+     else if (compare === 'snow'){
+           return 'Is snowboarding considered one word?'
+     }
+     else if( compare === 'wind'){
+            return 'Outside? Never heard of it.'
+     }
+     else {
+           return "idk Bro";
+     }
 
 
 
-
-
-
+}
 
 //handlebars function to populate form
     function showData(data){
@@ -46,11 +85,13 @@ function errorHandler(err){
           icon: data.currently.icon,
           summary: data.currently.summary,
           time: moment().format('LT'),
-          temp: data.currently.temperature
+          temp: data.currently.temperature,
+          todo: whatYouDo(data.currently.icon),
         };
         var html = template(data);
         $('#output').html(html);
-        console.log(data);
+        
+
 
     };
 
